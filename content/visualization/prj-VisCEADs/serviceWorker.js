b9ce57a76a68fb484192a49e490ea25b990008eb
Han-Teng Liao <hanteng@gmail.com>
2024-06-19 23:44:56 +0800

    base to https://oxon8.netlify.app/visualization/prj-visCEADs/
    https://oxon8.netlify.app/visualization/prj-visCEADs/

--- content/visualization/prj-VisCEADs/serviceWorker.js
+++ content/visualization/prj-VisCEADs/serviceWorker.js
@@ -1,5 +1,5 @@
 const appName = 'Panel Pyodide App'
-const appCacheName = 'Panel Pyodide App-c2917144bf344c169e6fb451770dc56d';
+const appCacheName = 'Panel Pyodide App-d2bfc96d35534294951d358580c25b55';
 
 const preCacheFiles = ['images/favicon.ico', 'images/icon-vector.svg', 'images/icon-32x32.png', 'images/icon-192x192.png', 'images/icon-512x512.png', 'images/apple-touch-icon.png', 'images/index_background.png'];
 
