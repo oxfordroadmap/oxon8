0cb4b48c1b62749a44a8c6b972ae5150f0e95e2b
Han-Teng Liao <hanteng@gmail.com>
2024-06-23 15:29:25 +0800

    20240623 update with accordian color fixed and tabs header improved with icons
    20240623 update with accordian color fixed and tabs header improved with icons

--- content/visualization/prj-VisCEADs/serviceWorker.js
+++ content/visualization/prj-VisCEADs/serviceWorker.js
@@ -1,5 +1,5 @@
 const appName = 'Panel Pyodide App'
-const appCacheName = 'Panel Pyodide App-40038073a4924fc4bce0948045bb739c';
+const appCacheName = 'Panel Pyodide App-e245bce6771642c092956e0990c34eca';
 
 const preCacheFiles = ['images/favicon.ico', 'images/icon-vector.svg', 'images/icon-32x32.png', 'images/icon-192x192.png', 'images/icon-512x512.png', 'images/apple-touch-icon.png', 'images/index_background.png'];
 
